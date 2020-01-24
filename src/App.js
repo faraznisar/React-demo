33a6c2cb1969b45577792f829432afb8259afc13
Shah <fshah7@dxc.com>
2020-01-24 22:20:36 +0530

    Added GoalList and NewGoal components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './App.css'
+import NewGoal from "./components/NewGoal/NewGoal"
+import GoalList from './components/GoalList/GoalList'
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const courseGoals = [
+    { id: 1, text: 'Finish the course' },
+    { id: 2, text: 'Make use of functional rather than class components' },
+    { id: 3, text: 'Hopefully goes well.' },
+  ]
+
+  return <div className="course-goals">
+    <h2>Course Goals</h2>
+    <NewGoal />
+    <GoalList goals={courseGoals} />
+
+  </div>
 };
 
 export default App;
